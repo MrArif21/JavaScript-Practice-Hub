ceccdcc1c0a3a539d6f7d49d1a679ec8a5176927
MrArif21 <mr.mohdarif21@gmail.com>
2024-02-27 23:51:47 +0530

    first commit

---
+++ 1QA.js
@@ -0,0 +1,2 @@
+let foot = "hello"
+console.log (foot)
---
+++ 2QA.js
@@ -0,0 +1,22 @@
+// loop print 1 to 5
+
+console.log("using a for loop");
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("using a While loop");
+
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+console.log("using a Do-While loop");
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 5);
